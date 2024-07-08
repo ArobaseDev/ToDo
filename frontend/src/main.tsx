0c6ce1bfa772a738bbddb04b28965d73c18d05d1
Aro Dev <arodev@arobase-pointcom.fr>
2024-07-08 22:50:24 +0200

    Router et recupération backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import TodoList from './pages/TodoList.tsx'
+import Register from './pages/Register.tsx'
 import App from './App.tsx'
 import './assets/css/index.css'
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/register', element: <Register /> },
+  { path: '/todos-list', element: <TodoList /> },
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
