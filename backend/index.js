8aef1b67041529100ce11f350984ab2be9b9a3b3
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-08 22:31:56 +0200

    Updated index.js and todo.db

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,43 @@ app.get('/', (req, res) => {
 // Connection à la base de données SQLite
 const db = new sqlite3.Database('./todo.db', (err) => {
   if (err) {
-    console.error(err.message);
-    throw err;
+    console.error("Error opening database" + err.message);
+  } else {
+
+    db.run(`CREATE TABLE IF NOT EXISTS User (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      email TEXT UNIQUE NOT NULL
+    )`, (err) => {
+      if (err) {
+        console.log("Table User already exists.");
+      }
+      let insert = `INSERT INTO User (username, password, email) VALUES (?,?,?)`;
+      db.run(insert, ['hadil', 'hadil932', 'hadil@example.com']);
+      db.run(insert, ['desire', 'desire921', 'desire@example.com']);
+      db.run(insert, ['kevin', 'kevin910', 'kevin@example.com']);
+    });
+
+    db.run(`CREATE TABLE IF NOT EXISTS Todo (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT,
+      is_done BOOLEAN DEFAULT 0,
+      createdAt TIMESTAMP,
+      finishedAt TIMESTAMP,
+      userId INTEGER NOT NULL
+      FOREIGN KEY(userId) REFERENCES User(id)
+    )`, (err) => {
+      if (err) {
+        console.log("Table Todo already exists.");
+      }
+      let insert = `INSERT INTO Todo (title, description, is_done, createdAt, finishedAt, userId) VALUES (?,?,?,?,?,?)`;
+      db.run(insert, ['A faire', 'Faire des chocolats', 0, new Date(), null, 1]);
+      db.run(insert, ['A faire', 'Faire des pommes', 0, new Date(), null, 2]);
+      db.run(insert, ['A faire', 'Faire des croissants', 0, new Date(), null, 3]);
+    });
   }
-  console.log('Connected to the SQLite database.');
 });
 
 // Lancement du serveur sur le port 3000
