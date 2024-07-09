d81e5a04adf90c97a76f6213bbf32d34c35ce8df
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-10 00:07:16 +0200

    Updated User.js and Task.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,4 @@
 const { Sequelize } = require("sequelize");
-// import sequelize from "../config/database.js";
-// import bdd from "../config/database.js";
 
 // init DataTypes
 const { DataTypes } = Sequelize;
